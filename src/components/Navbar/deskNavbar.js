0353644be465e7e617d6de87e41d97c79fde1270
Zach Davis <zacharydavis92@gmail.com>
2022-06-18 12:28:40 -0500

    'deploy routes test'

--- src/components/Navbar/deskNavbar.js
+++ src/components/Navbar/deskNavbar.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <ul className="flex-wrap justify-evenly text-sky-800 hidden md:flex">
-            <Link to='/' className="text-2xl m-2 md:text-xl hover:font-bold">About Me</Link>
-            <Link to='/portfolio' className="text-2xl m-2 md:text-xl hover:font-bold">Portfolio</Link>
-            <Link to='/contact' className="text-2xl m-2 md:text-xl hover:font-bold">Contact Me</Link>
-            <Link to='/resume' className="text-2xl m-2 md:text-xl hover:font-bold">Resume</Link>
+            <Link to='/Upgraded-Portfolio/' className="text-2xl m-2 md:text-xl hover:font-bold">About Me</Link>
+            <Link to='/Upgraded-Portfolio/Portfolio' className="text-2xl m-2 md:text-xl hover:font-bold">Portfolio</Link>
+            <Link to='/Upgraded-Portfolio/Contact' className="text-2xl m-2 md:text-xl hover:font-bold">Contact Me</Link>
+            <Link to='/Upgraded-Portfolio/Resume' className="text-2xl m-2 md:text-xl hover:font-bold">Resume</Link>
         </ul>
     );
 };
--- src/components/Navbar/mobileNav.js
+++ src/components/Navbar/mobileNav.js
@@ -22,10 +22,10 @@ const MobileNavbar = () => {
             ):(
             <img className="w-10 mx-auto my-auto border-2 shadow hover:cursor-pointer md:hidden" alt="" onClick={openNav} src={menuIcon}></img>)}
             {open && <ul className="absolute top-24 right-0 bg-white opacity-80 hover:opacity-100 text-sky-800 rounded-md shadow flex flex-col items-end md:hidden">
-                <Link to='/' className="text-2xl m-2 md:text-xl hover:font-bold" onClick={closeNav}>About Me</Link>
-                <Link to='/portfolio' className="text-2xl m-2 md:text-xl hover:font-bold" onClick={closeNav}>Portfolio</Link>
-                <Link to='/contact' className="text-2xl m-2 md:text-xl hover:font-bold" onClick={closeNav}>Contact Me</Link>
-                <Link to='/resume' className="text-2xl m-2 md:text-xl hover:font-bold" onClick={closeNav}>Resume</Link>
+                <Link to='/Upgraded-Portfolio/' className="text-2xl m-2 md:text-xl hover:font-bold" onClick={closeNav}>About Me</Link>
+                <Link to='/Upgraded-Portfolio/Portfolio' className="text-2xl m-2 md:text-xl hover:font-bold" onClick={closeNav}>Portfolio</Link>
+                <Link to='/Upgraded-Portfolio/Contact' className="text-2xl m-2 md:text-xl hover:font-bold" onClick={closeNav}>Contact Me</Link>
+                <Link to='/Upgraded-Portfolio/Resume' className="text-2xl m-2 md:text-xl hover:font-bold" onClick={closeNav}>Resume</Link>
             </ul>}
         </div>
     );
